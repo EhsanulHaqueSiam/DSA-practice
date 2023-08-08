246a7b792461f20b354c598c0dbd364c194e3203
EhsanulHaqueSiam <ehsanul.siamdev@gmail.com>
2023-08-08 23:22:47 +0600

    Update RecursiveInsert-DeleteonBST.cpp

--- Data Structure/Trees/Binary-Search-Tree/RecursiveInsert-DeleteonBST.cpp
+++ Data Structure/Trees/Binary-Search-Tree/RecursiveInsert-DeleteonBST.cpp
@@ -1,282 +1,257 @@
 #include <iostream>
 
-namespace google_style {
-
-// Node class for binary search tree
-class Node {
- public:
-  Node* left_child;
-  int data;
-  Node* right_child;
+class TreeNode {
+public:
+    TreeNode* left_child;
+    int value;
+    TreeNode* right_child;
 };
 
-// BinarySearchTree class with various operations
 class BinarySearchTree {
- private:
-  Node* root;
+private:
+    TreeNode* root;
 
- public:
-  BinarySearchTree() { root = nullptr; }
-  
-  // Get the root node of the tree
-  Node* GetRoot() { return root; }
-  
-  // Iteratively insert a key into the tree
-  void IterativeInsert(int key);
-  
-  // Perform in-order traversal of the tree
-  void InorderTraversal(Node* node);
-  
-  // Iteratively search for a key in the tree
-  Node* IterativeSearch(int key);
-  
-  // Recursively insert a key into the tree
-  Node* RecursiveInsert(Node* node, int key);
-  
-  // Recursively search for a key in the tree
-  Node* RecursiveSearch(Node* node, int key);
-  
-  // Delete a node with a specific key from the tree
-  Node* DeleteNode(Node* node, int key);
-  
-  // Calculate the height of a node in the tree
-  int CalculateHeight(Node* node);
-  
-  // Find the in-order predecessor of a node
-  Node* InorderPredecessor(Node* node);
-  
-  // Find the in-order successor of a node
-  Node* InorderSuccessor(Node* node);
+public:
+    BinarySearchTree() { root = nullptr; }
+
+    TreeNode* GetRoot() { return root; }
+
+    // Iteratively insert a key into the BST
+    void Insert(int key);
+
+    // Inorder traversal of the BST
+    void InorderTraversal(TreeNode* node);
+
+    // Iteratively search for a key in the BST
+    TreeNode* Search(int key);
+
+    // Recursively insert a key into the BST
+    TreeNode* RecursiveInsert(TreeNode* node, int key);
+
+    // Recursively search for a key in the BST
+    TreeNode* RecursiveSearch(TreeNode* node, int key);
+
+    // Delete a key from the BST
+    TreeNode* Delete(TreeNode* node, int key);
+
+    // Calculate the height of a node
+    int CalculateHeight(TreeNode* node);
+
+    // Find the inorder predecessor of a node
+    TreeNode* InorderPredecessor(TreeNode* node);
+
+    // Find the inorder successor of a node
+    TreeNode* InorderSuccessor(TreeNode* node);
 };
 
-// Function to iteratively insert a key into the tree
-void BinarySearchTree::IterativeInsert(int key) {
-  Node* current = root;
-  Node* parent = nullptr;
+void BinarySearchTree::Insert(int key) {
+    TreeNode* temp = root;
+    TreeNode* parent = nullptr;
 
-  // Handle case when root is empty
-  if (root == nullptr) {
-    Node* new_node = new Node;
-    new_node->data = key;
+    // If the tree is empty
+    if (root == nullptr) {
+        TreeNode* new_node = new TreeNode;
+        new_node->value = key;
+        new_node->left_child = nullptr;
+        new_node->right_child = nullptr;
+        root = new_node;
+        return;
+    }
+
+    while (temp != nullptr) {
+        parent = temp;
+        if (key < temp->value) {
+            temp = temp->left_child;
+        } else if (key > temp->value) {
+            temp = temp->right_child;
+        } else {
+            return; // Key already exists in the tree
+        }
+    }
+
+    // Now temp points at nullptr and parent points at the insertion location
+    TreeNode* new_node = new TreeNode;
+    new_node->value = key;
     new_node->left_child = nullptr;
     new_node->right_child = nullptr;
-    root = new_node;
-    return;
-  }
 
-  while (current != nullptr) {
-    parent = current;
-    if (key < current->data) {
-      current = current->left_child;
-    } else if (key > current->data) {
-      current = current->right_child;
+    if (key < parent->value) {
+        parent->left_child = new_node;
     } else {
-      return;  // Key already exists, no need to insert
+        parent->right_child = new_node;
     }
-  }
-
-  // Now 'current' points to nullptr and 'parent' points to the insert location
-  Node* new_node = new Node;
-  new_node->data = key;
-  new_node->left_child = nullptr;
-  new_node->right_child = nullptr;
-
-  if (key < parent->data) {
-    parent->left_child = new_node;
-  } else {
-    parent->right_child = new_node;
-  }
 }
 
-// Function to perform in-order traversal of the tree
-void BinarySearchTree::InorderTraversal(Node* node) {
-  if (node) {
-    InorderTraversal(node->left_child);
-    std::cout << node->data << ", ";
-    InorderTraversal(node->right_child);
-  }
-}
-
-// Function to iteratively search for a key in the tree
-Node* BinarySearchTree::IterativeSearch(int key) {
-  Node* current = root;
-  while (current != nullptr) {
-    if (key == current->data) {
-      return current;
-    } else if (key < current->data) {
-      current = current->left_child;
-    } else {
-      current = current->right_child;
+void BinarySearchTree::InorderTraversal(TreeNode* node) {
+    if (node) {
+        InorderTraversal(node->left_child);
+        std::cout << node->value << ", ";
+        InorderTraversal(node->right_child);
     }
-  }
-  return nullptr;
 }
 
-// Function to recursively insert a key into the tree
-Node* BinarySearchTree::RecursiveInsert(Node* node, int key) {
-  if (node == nullptr) {
-    Node* new_node = new Node;
-    new_node->data = key;
-    new_node->left_child = nullptr;
-    new_node->right_child = nullptr;
-    return new_node;
-  }
-
-  if (key < node->data) {
-    node->left_child = RecursiveInsert(node->left_child, key);
-  } else if (key > node->data) {
-    node->right_child = RecursiveInsert(node->right_child, key);
-  }
-  return node;  // Key already exists in the tree
-}
-
-// Function to recursively search for a key in the tree
-Node* BinarySearchTree::RecursiveSearch(Node* node, int key) {
-  if (node == nullptr) {
+TreeNode* BinarySearchTree::Search(int key) {
+    TreeNode* temp = root;
+    while (temp != nullptr) {
+        if (key == temp->value) {
+            return temp;
+        } else if (key < temp->value) {
+            temp = temp->left_child;
+        } else {
+            temp = temp->right_child;
+        }
+    }
     return nullptr;
-  }
+}
 
-  if (key == node->data) {
+TreeNode* BinarySearchTree::RecursiveInsert(TreeNode* node, int key) {
+    TreeNode* new_node;
+    if (node == nullptr) {
+        new_node = new TreeNode;
+        new_node->value = key;
+        new_node->left_child = nullptr;
+        new_node->right_child = nullptr;
+        return new_node;
+    }
+
+    if (key < node->value) {
+        node->left_child = RecursiveInsert(node->left_child, key);
+    } else if (key > node->value) {
+        node->right_child = RecursiveInsert(node->right_child, key);
+    }
     return node;
-  } else if (key < node->data) {
-    return RecursiveSearch(node->left_child, key);
-  } else {
-    return RecursiveSearch(node->right_child, key);
-  }
 }
 
-// Function to delete a node with a specific key from the tree
-Node* BinarySearchTree::DeleteNode(Node* node, int key) {
-  Node* temp;
-
-  if (node == nullptr) {
-    return nullptr;
-  }
-
-  if (node->left_child == nullptr && node->right_child == nullptr) {
-    if (node == root) {
-      root = nullptr;
+TreeNode* BinarySearchTree::RecursiveSearch(TreeNode* node, int key) {
+    if (node == nullptr) {
+        return nullptr;
     }
-    delete node;
-    return nullptr;
-  }
 
-  if (key < node->data) {
-    node->left_child = DeleteNode(node->left_child, key);
-  } else if (key > node->data) {
-    node->right_child = DeleteNode(node->right_child, key);
-  } else {
-    if (CalculateHeight(node->left_child) > CalculateHeight(node->right_child)) {
-      temp = InorderPredecessor(node->left_child);
-      node->data = temp->data;
-      node->left_child = DeleteNode(node->left_child, temp->data);
+    if (key == node->value) {
+        return node;
+    } else if (key < node->value) {
+        return RecursiveSearch(node->left_child, key);
     } else {
-      temp = InorderSuccessor(node->right_child);
-      node->data = temp->data;
-      node->right_child = DeleteNode(node->right_child, temp->data);
+        return RecursiveSearch(node->right_child, key);
     }
-  }
-  return node;
 }
 
-// Function to calculate the height of a node in the tree
-int BinarySearchTree::CalculateHeight(Node* node) {
-  int left_height, right_height;
-  if (node == nullptr) {
-    return 0;
-  }
-  left_height = CalculateHeight(node->left_child);
-  right_height = CalculateHeight(node->right_child);
-  return left_height > right_height ? left_height + 1 : right_height + 1;
+TreeNode* BinarySearchTree::Delete(TreeNode* node, int key) {
+    TreeNode* q;
+
+    if (node == nullptr) {
+        return nullptr;
+    }
+
+    if (node->left_child == nullptr && node->right_child == nullptr) {
+        if (node == root) {
+            root = nullptr;
+        }
+        delete node;
+        return nullptr;
+    }
+
+    if (key < node->value) {
+        node->left_child = Delete(node->left_child, key);
+    } else if (key > node->value) {
+        node->right_child = Delete(node->right_child, key);
+    } else {
+        if (CalculateHeight(node->left_child) > CalculateHeight(node->right_child)) {
+            q = InorderPredecessor(node->left_child);
+            node->value = q->value;
+            node->left_child = Delete(node->left_child, q->value);
+        } else {
+            q = InorderSuccessor(node->right_child);
+            node->value = q->value;
+            node->right_child = Delete(node->right_child, q->value);
+        }
+    }
+    return node;
 }
 
-// Function to find the in-order predecessor of a node
-Node* BinarySearchTree::InorderPredecessor(Node* node) {
-  while (node && node->right_child != nullptr) {
-    node = node->right_child;
-  }
-  return node;
+int BinarySearchTree::CalculateHeight(TreeNode* node) {
+    int left_height;
+    int right_height;
+    if (node == nullptr) {
+        return 0;
+    }
+    left_height = CalculateHeight(node->left_child);
+    right_height = CalculateHeight(node->right_child);
+    return left_height > right_height ? left_height + 1 : right_height + 1;
 }
 
-// Function to find the in-order successor of a node
-Node* BinarySearchTree::InorderSuccessor(Node* node) {
-  while (node && node->left_child != nullptr) {
-    node = node->left_child;
-  }
-  return node;
+TreeNode* BinarySearchTree::InorderPredecessor(TreeNode* node) {
+    while (node && node->right_child != nullptr) {
+        node = node->right_child;
+    }
+    return node;
 }
 
-}  
+TreeNode* BinarySearchTree::InorderSuccessor(TreeNode* node) {
+    while (node && node->left_child != nullptr) {
+        node = node->left_child;
+    }
+    return node;
+}
 
 int main() {
-  // Create a binary search tree instance
-  google_style::BinarySearchTree bst;
+    BinarySearchTree bst;
 
-  // Iteratively insert keys into the tree
-  bst.IterativeInsert(10);
-  bst.IterativeInsert(5);
-  bst.IterativeInsert(20);
-  bst.IterativeInsert(8);
-  bst.IterativeInsert(30);
+    // Iterative insert
+    bst.Insert(10);
+    bst.Insert(5);
+    bst.Insert(20);
+    bst.Insert(8);
+    bst.Insert(30);
 
-  // Perform in-order traversal of the tree
-  bst.InorderTraversal(bst.GetRoot());
-  std::cout << std::endl;
+    // Inorder traversal
+    bst.InorderTraversal(bst.GetRoot());
+    std::cout << std::endl;
 
-    // Iteratively search for a key in the tree
-  google_style::Node* temp = bst.IterativeSearch(2);
-  if (temp != nullptr) {
-    std::cout << "Iterative Search: Key " << temp->data << " found." << std::endl;
-  } else {
-    std::cout << "Iterative Search: Element not found." << std::endl;
-  }
+    // Iterative search
+    TreeNode* temp = bst.Search(2);
+    if (temp != nullptr) {
+        std::cout << temp->value << std::endl;
+    } else {
+        std::cout << "Element not found" << std::endl;
+    }
 
-  // Recursively search for a key in the tree
-  temp = bst.RecursiveSearch(bst.GetRoot(), 20);
-  if (temp != nullptr) {
-    std::cout << "Recursive Search: Key " << temp->data << " found." << std::endl;
-  } else {
-    std::cout << "Recursive Search: Element not found." << std::endl;
-  }
+    // Recursive search
+    temp = bst.RecursiveSearch(bst.GetRoot(), 20);
+    if (temp != nullptr) {
+        std::cout << temp->value << std::endl;
+    } else {
+        std::cout << "Element not found" << std::endl;
+    }
 
-  // Recursively insert keys into the tree
-  bst.RecursiveInsert(bst.GetRoot(), 50);
-  bst.RecursiveInsert(bst.GetRoot(), 70);
-  bst.RecursiveInsert(bst.GetRoot(), 1);
+    // Recursive insert
+    bst.RecursiveInsert(bst.GetRoot(), 50);
+    bst.RecursiveInsert(bst.GetRoot(), 70);
+    bst.RecursiveInsert(bst.GetRoot(), 1);
+    bst.InorderTraversal(bst.GetRoot());
+    std::cout << "\n" << std::endl;
 
-  // Perform in-order traversal of the tree
-  std::cout << "In-order Traversal after Recursive Insertions: ";
-  bst.InorderTraversal(bst.GetRoot());
-  std::cout << std::endl;
+    // Inorder predecessor and inorder successor
+    BinarySearchTree bs;
+    bs.Insert(5);
+    bs.Insert(2);
+    bs.Insert(8);
+    bs.Insert(7);
+    bs.Insert(9);
+    bs.Insert(1);
 
-  // Create another binary search tree instance
-  google_style::BinarySearchTree bs;
-  bs.IterativeInsert(5);
-  bs.IterativeInsert(2);
-  bs.IterativeInsert(8);
-  bs.IterativeInsert(7);
-  bs.IterativeInsert(9);
-  bs.IterativeInsert(1);
+    temp = bs.InorderPredecessor(bs.GetRoot());
+    std::cout << "InPre: " << temp->value << std::endl;
 
-  // Find the in-order predecessor and successor
-  temp = bs.InorderPredecessor(bs.GetRoot());
-  std::cout << "In-order Predecessor: " << temp->data << std::endl;
+    temp = bs.InorderSuccessor(bs.GetRoot());
+    std::cout << "InSucc: " << temp->value << std::endl;
 
-  temp = bs.InorderSuccessor(bs.GetRoot());
-  std::cout << "In-order Successor: " << temp->data << std::endl;
+    bs.InorderTraversal(bs.GetRoot());
+    std::cout << std::endl;
 
-  // Perform in-order traversal of the tree
-  std::cout << "In-order Traversal: ";
-  bs.InorderTraversal(bs.GetRoot());
-  std::cout << std::endl;
+    // Delete
+    bs.Delete(bs.GetRoot(), 7);
+    bs.InorderTraversal(bs.GetRoot());
 
-  // Delete a node from the tree
-  bs.DeleteNode(bs.GetRoot(), 7);
-
-  // Perform in-order traversal after deletion
-  std::cout << "In-order Traversal after Deletion: ";
-  bs.InorderTraversal(bs.GetRoot());
-  std::cout << std::endl;
-
-  return 0;
+    return 0;
 }
