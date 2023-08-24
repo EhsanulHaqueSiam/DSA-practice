852f3d209c5dab1cdd0c0db407ea4806cee8352e
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-08-24 04:24:00 -0700

    Update InsertinHeap.cpp

--- Data Structure/Heap/InsertinHeap.cpp
+++ Data Structure/Heap/InsertinHeap.cpp
@@ -2,60 +2,68 @@
 #include <vector>
 
 using namespace std;
- 
-// Lecture based
-void InsertA(int A[], int n){
+
+// Inserts an element into a max heap defined in a raw array.
+// The process involves placing the element at the end of the heap
+// and then bubbling it up to its correct position.
+void InsertA(int A[], int n) {
     int i = n;
     int temp = A[n];
-    while (i > 0 && temp > A[i % 2 == 0 ? (i/2)-1 : i/2]){
-        A[i] = A[i % 2 == 0 ? (i/2)-1 : i/2];
-        i = i % 2 == 0 ? (i/2)-1 : i/2;
+
+    // Check if the new node's parent is smaller. If so, then swap.
+    // The check for even/odd is to handle 0-based index array.
+    while (i > 0 && temp > A[i % 2 == 0 ? (i / 2) - 1 : i / 2]) {
+        A[i] = A[i % 2 == 0 ? (i / 2) - 1 : i / 2];
+        i = i % 2 == 0 ? (i / 2) - 1 : i / 2;
     }
     A[i] = temp;
 }
- 
- 
-// STL vector based
-void Insert(vector<int>& vec, int key){
-    // Insert key at the end
+
+// Inserts an element into a max heap defined using an STL vector.
+void Insert(vector<int>& vec, int key) {
+    // Insert key at the end of the vector.
     auto i = vec.size();
     vec.emplace_back(key);
- 
-    // Rearrange elements: Indices are not DRY :-(
-    while (i > 0 && key > vec[i % 2 == 0 ? (i/2)-1 : i/2]){
-        vec[i] = vec[i % 2 == 0 ? (i/2)-1 : i/2];
-        i = i % 2 == 0 ? (i/2)-1 : i/2;
+
+    // Bubble up the inserted element to its correct position.
+    while (i > 0 && key > vec[i % 2 == 0 ? (i / 2) - 1 : i / 2]) {
+        vec[i] = vec[i % 2 == 0 ? (i / 2) - 1 : i / 2];
+        i = i % 2 == 0 ? (i / 2) - 1 : i / 2;
     }
     vec[i] = key;
 }
- 
+
+// Utility function to print the elements of the heap.
 template <class T>
-void Print(T& vec, int n){
-    cout << "Max Heap: [" << flush;
-    for (int i=0; i<n; i++){
-        cout << vec[i] << flush;
-        if (i < n-1){
-            cout << ", " << flush;
+void Print(const T& vec, int n) {
+    cout << "Max Heap: [";
+    for (int i = 0; i < n; i++) {
+        cout << vec[i];
+        if (i < n - 1) {
+            cout << ", ";
         }
     }
     cout << "]" << endl;
 }
- 
- 
+
 int main() {
- 
     int a[] = {45, 35, 15, 30, 10, 12, 6, 5, 20, 50};
+
+    // Insert 50 into the heap defined in the raw array.
     InsertA(a, 9);
-    Print(a, sizeof(a)/sizeof(a[0]));
+    Print(a, sizeof(a) / sizeof(a[0]));
     cout << endl;
- 
-    // STL based
+
+    // Using STL vector to represent heap.
     vector<int> v = {45, 35, 15, 30, 10, 12, 6, 5, 20};
     Print(v, v.size());
-    v.reserve(15);  // Reserve space for 15 elements
- 
+
+    // Reserve space for potential additional elements.
+    v.reserve(15);
+
+    // Insert 50 into the heap defined in the STL vector.
     Insert(v, 50);
     Print(v, v.size());
- 
+
     return 0;
 }
