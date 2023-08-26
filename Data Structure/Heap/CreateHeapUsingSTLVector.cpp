5cd7dfe89ec75c57e8b9a7f741e4367bf66a169a
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-08-26 22:34:24 +0600

    Update CreateHeapUsingSTLVector.cpp

--- Data Structure/Heap/CreateHeapUsingSTLVector.cpp
+++ Data Structure/Heap/CreateHeapUsingSTLVector.cpp
@@ -2,70 +2,75 @@
 #include <vector>
 
 using namespace std;
- 
-void Insert(vector<int>& vec, int key){
-    // Insert key at the end
-    auto i = vec.size();
-    vec.emplace_back(key);
- 
-    // Rearrange elements: O(log n)
-    while (i > 0 && key > vec[i % 2 == 0 ? (i/2)-1 : i/2]){
-        vec[i] = vec[i % 2 == 0 ? (i/2)-1 : i/2];
-        i = i % 2 == 0 ? (i/2)-1 : i/2;
-    }
-    vec[i] = key;
+
+// Function to insert a key into the max heap represented as a vector.
+// The function ensures that the heap property is maintained after every insert.
+void Insert(vector<int>& heap, int key) {
+  // Insert the key at the end of the heap.
+  int i = heap.size();
+  heap.emplace_back(key);
+
+  // Rearrange elements to maintain the heap property. 
+  // We compare the inserted key with its parent and move it up the heap as required.
+  while (i > 0 && key > heap[i % 2 == 0 ? (i / 2) - 1 : i / 2]) {
+    heap[i] = heap[i % 2 == 0 ? (i / 2) - 1 : i / 2];
+    i = i % 2 == 0 ? (i / 2) - 1 : i / 2;
+  }
+  heap[i] = key;
 }
- 
-void InsertInplace(int A[], int n){
-    int i = n;
-    int temp = A[n];
-    while (i > 0 && temp > A[i % 2 == 0 ? (i/2)-1 : i/2]){
-        A[i] = A[i % 2 == 0 ? (i/2)-1 : i/2];
-        i = i % 2 == 0 ? (i/2)-1 : i/2;
-    }
-    A[i] = temp;
+
+// In-place version of the above function for an array.
+void InsertInplace(int arr[], int n) {
+  int i = n;
+  int temp = arr[n];
+
+  while (i > 0 && temp > arr[i % 2 == 0 ? (i / 2) - 1 : i / 2]) {
+    arr[i] = arr[i % 2 == 0 ? (i / 2) - 1 : i / 2];
+    i = i % 2 == 0 ? (i / 2) - 1 : i / 2;
+  }
+  arr[i] = temp;
 }
- 
-void CreateHeap(vector<int>& vec, int A[], int n){
-    // O(n log n)
-    for (int i=0; i<n; i++){
-        Insert(vec, A[i]);
-    }
+
+// Function to create a max heap from an array.
+// This function inserts each element of the array into the heap.
+void CreateHeap(vector<int>& heap, int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    Insert(heap, arr[i]);
+  }
 }
- 
-void createHeap(int A[], int n){
-    for (int i=0; i<n; i++){
-        InsertInplace(A, i);
-    }
+
+// In-place version of the above function.
+void createHeap(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    InsertInplace(arr, i);
+  }
 }
- 
+
+// Template function to print arrays and vectors.
 template <class T>
-void Print(T& vec, int n, char c){
-    cout << c << ": [" << flush;
-    for (int i=0; i<n; i++){
-        cout << vec[i] << flush;
-        if (i < n-1){
-            cout << ", " << flush;
-        }
+void Print(T& container, int n, char label) {
+  cout << label << ": [";
+  for (int i = 0; i < n; i++) {
+    cout << container[i];
+    if (i < n - 1) {
+      cout << ", ";
     }
-    cout << "]" << endl;
+  }
+  cout << "]" << endl;
 }
- 
- 
+
 int main() {
- 
-    cout << "Create Heap" << endl;
-    int b[] = {10, 20, 30, 25, 5, 40, 35};
-    Print(b, sizeof(b)/sizeof(b[0]), 'b');
- 
-    vector<int> v;
-    CreateHeap(v, b, sizeof(b)/sizeof(b[0]));
-    Print(v, v.size(), 'v');
- 
-    cout << "Inplace Insert" << endl;
-    createHeap(b, sizeof(b)/sizeof(b[0]));
-    Print(b, sizeof(b)/sizeof(b[0]), 'b');
- 
- 
-    return 0;
+  cout << "Create Heap" << endl;
+  int b[] = {10, 20, 30, 25, 5, 40, 35};
+  Print(b, sizeof(b) / sizeof(b[0]), 'b');
+
+  vector<int> v;
+  CreateHeap(v, b, sizeof(b) / sizeof(b[0]));
+  Print(v, v.size(), 'v');
+
+  cout << "Inplace Insert" << endl;
+  createHeap(b, sizeof(b) / sizeof(b[0]));
+  Print(b, sizeof(b) / sizeof(b[0]), 'b');
+
+  return 0;
 }
